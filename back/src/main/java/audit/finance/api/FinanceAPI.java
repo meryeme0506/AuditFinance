09438afee4d6c2c5b7a756f4595e68c2dc789138
Meryeme Haman <84973134+meryeme0506@users.noreply.github.com>
2023-01-12 00:49:54 +0100

    update FinanceAPI

--- back/src/main/java/audit/finance/api/FinanceAPI.java
+++ back/src/main/java/audit/finance/api/FinanceAPI.java
@@ -7,9 +7,7 @@ import audit.finance.repository.FinanceEntityRepository;
 import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDate;
-import java.time.ZoneId;
-import java.time.format.DateTimeFormatter;
+import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
@@ -22,29 +20,36 @@ public class FinanceAPI {
     private final FinanceEntityRepository financeEntityRepository;
 
     @GetMapping("/stocks")
-    public List<Finance> getAll(){
+    public List<Finance> getAll() {
         return financeEntityRepository.findAll();
     }
 
     @GetMapping("/stocks/key={stockKey}")
-    public List<Finance> findByStockKey(@PathVariable("stockKey") String stockKey){
+    public List<Finance> findByStockKey(@PathVariable("stockKey") String stockKey) {
         return financeEntityRepository.findByStockKey(stockKey);
     }
 
     @GetMapping("/stocks/date={dateTarget}")
-    public List<Finance> findByDateTarget(@PathVariable("dateTarget") String dateTarget){
-        LocalDate localDate = null;
+    public List<Finance> findByDateTarget(@PathVariable("dateTarget") String dateTarget) {
         Date date = new Date();
-        try{
-             localDate = LocalDate.parse(dateTarget, DateTimeFormatter.ISO_DATE);
-             date=Date.from(localDate.atStartOfDay(ZoneId.systemDefault()).toInstant())
-        }catch (IllegalArgumentException e) {
-            System.out.println("Exception: " + e);
+        try {
+            date = DateUtils.parseDate(dateTarget, "YYYY-MM-DD");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
         }
         return financeEntityRepository.findByDateTarget(date);
     }
 
     @GetMapping("/stocks/dateFrom={dateFrom}&dateTo={dateTo}")
-    public List<Finance> findByBetween(@PathVariable("dateFrom") String dateFrom,@PathVariable("dateTo") String dateTo){
-        return financeEntityRepository.findByBetween();
+    public List<Finance> findByBetween(@PathVariable("dateFrom") String dateFrom, @PathVariable("dateTo") String dateTo) {
+        Date date1 = new Date();
+        Date date2 = new Date();
+        try {
+            date1 = DateUtils.parseDate(dateFrom, "YYYY-MM-DD");
+            date2 = DateUtils.parseDate(dateTo, "YYYY-MM-DD");
+        } catch (ParseException e) {
+            throw new RuntimeException(e);
+        }
+        return financeEntityRepository.findByBetween(date1, date2);
     }
+}
